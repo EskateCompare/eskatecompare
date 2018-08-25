c4a33d7cb0d78750f482f1b5bed85cb9eee84af4
Warren Phen <warren.phen@gmail.com>
2018-08-25 00:45:10 -0400

    map out filter options

--- client/src/components/Containers/CompareProducts/Filter.js
+++ client/src/components/Containers/CompareProducts/Filter.js
@@ -6,7 +6,7 @@ class Filter extends Component {
   constructor(props){
     super(props);
       this.state = {
-        filterOptions: [],
+        filter: {},
         value: 10
     }
   }
@@ -17,58 +17,40 @@ class Filter extends Component {
     })
   };
 
+
+
   componentDidMount() {
-    fetch(`http://localhost:3000/api/filter-options/`)
+    fetch(`http://localhost:3000/api/filter-options`)
       .then(response => response.json())
-      .then(json => this.setState({filterOptions: json.filter}))
+      .then(json => this.setState({filter: json.stats.filterOptions}))
+  }
+
+  renderFilterOptions() {
+    const { filter } = this.state;
+
+    const filterItems = filter.map((option, index) => {
+      return (
+        <Form.Group key={index} grouped>
+          <label>{option.title}</label>
+          { filter.title === 'brand' ? <Form.Field icon='search' control={Input} type='input' placeholder='Search Brands'/> : null}
+          { filter.options.map((value) => <Form.Field label={value.label} control={Checkbox} type='checkbox' />) }
+        </Form.Group>
+        )
+      }
+    );
+
+    return filterItems;
   }
 
   render() {
     const { value } = this.state;
+    this.renderedFilterOptions = this.renderFilterOptions();
+    // console.log(this.state.filterOptions);
 
     return (
       <div>
         <Form>
-          <Form.Group grouped>
-            <label>Brand</label>
-            <Form.Field icon='search' control={Input} type='input' placeholder='Search Brands'/>
-            <Form.Field label='Boosted' control={Checkbox} type='checkbox' />
-            <Form.Field label='Inboard' control={Checkbox} type='checkbox' />
-          </Form.Group>
-          <Divider/>
-          <Form.Group grouped>
-            <label>Board Type</label>
-            <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
-            <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
-          </Form.Group>
-          <Divider/>
-          <Form.Group grouped>
-            <label>Price</label>
-              <Slider
-                min={0}
-                max={100}
-                value={value}
-                onChange={this.handleChange}
-              />
-          </Form.Group>
-          <Divider/> 
-          <Form.Group grouped>
-            <label>Max Speed</label>
-            <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
-            <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
-          </Form.Group>
-          <Divider/>
-          <Form.Group grouped>
-            <label>Range</label>
-            <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
-            <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
-          </Form.Group>
-          <Divider/>
-          <Form.Group grouped>
-            <label>Motor</label>
-            <Form.Field label='Belt' control={Checkbox} type='checkbox' />
-            <Form.Field label='Hub' control={Checkbox} type='checkbox' />
-          </Form.Group>
+          {this.renderedFilterOptions}
         </Form>   
       </div>
     );
@@ -76,3 +58,45 @@ class Filter extends Component {
 }
 
 export default Filter;
+
+
+// <Form.Group grouped>
+//             <label>Brand</label>
+//             <Form.Field icon='search' control={Input} type='input' placeholder='Search Brands'/>
+//             <Form.Field label='Boost' control={Checkbox} type='checkbox' />
+//             <Form.Field label='Inboard' control={Checkbox} type='checkbox' />
+//           </Form.Group>
+//           <Divider/>
+//           <Form.Group grouped>
+//             <label>Board Type</label>
+//             <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
+//             <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
+//           </Form.Group>
+//           <Divider/>
+//           <Form.Group grouped>
+//             <label>Price</label>
+//               <Slider
+//                 min={0}
+//                 max={100}
+//                 value={value}
+//                 onChange={this.handleChange}
+//               />
+//           </Form.Group>
+//           <Divider/> 
+//           <Form.Group grouped>
+//             <label>Max Speed</label>
+//             <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
+//             <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
+//           </Form.Group>
+//           <Divider/>
+//           <Form.Group grouped>
+//             <label>Range</label>
+//             <Form.Field label='Longboard' control={Checkbox} type='checkbox' />
+//             <Form.Field label='Pennyboard' control={Checkbox} type='checkbox' />
+//           </Form.Group>
+//           <Divider/>
+//           <Form.Group grouped>
+//             <label>Motor</label>
+//             <Form.Field label='Belt' control={Checkbox} type='checkbox' />
+//             <Form.Field label='Hub' control={Checkbox} type='checkbox' />
+//           </Form.Group>
