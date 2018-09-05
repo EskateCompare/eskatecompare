2f4b94c6f043ad4beffb1f0907e4dacd3937eb11
Warren Phen <warren.phen@gmail.com>
2018-09-05 01:05:06 -0400

    added show more/show less to filter options with greater than 5 options

--- server/client/src/components/Containers/CompareProducts/Filter.js
+++ server/client/src/components/Containers/CompareProducts/Filter.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { Form, Input, Checkbox, Header, Radio } from 'semantic-ui-react';
+import { Form, Input, Checkbox, Header, Radio, Icon, Divider } from 'semantic-ui-react';
 
 export default class Filter extends Component {
   constructor() {
     super()
+
+    this.state = {
+      showAllBrands: false,
+    }
 
     this.handleFilterSelect = this.handleFilterSelect.bind(this);
   }
@@ -21,12 +25,17 @@ export default class Filter extends Component {
     
     const filterItems = filter.map((option, index) => {
       return (
+        <div>
         <Form.Group key={index} grouped>
           <label><Header as='h4'>{option.displayTitle}</Header></label>
           { option.title === 'brands' ? <Form.Field icon='search' control={Input} type='input' placeholder='Search Brands'/> : null}
-          { option.formType === 'radio' ? option.options.map((value) => <Form.Field label={value.label} title={option.title} onChange={this.handleFilterSelect} control={Radio} type='radio' />) : null}
-          { option.formType === 'checkbox' ? option.options.map((value) => <Form.Field label={value.label} title={option.title} onChange={this.handleFilterSelect} control={Checkbox} type='checkbox' />) : null }
+          { option.formType === 'checkbox' ? option.options.slice(0, 5).map((value) => <Form.Field label={value.label} title={option.title} onChange={this.handleFilterSelect} control={Checkbox} type='checkbox' />) : null }
+          { this.state.showAllBrands ? option.options.slice(5, option.options.length).map((value) => <Form.Field label={value.label} title={option.title} onChange={this.handleFilterSelect} control={Checkbox} type='checkbox' />) : null}
+          { option.options.length > 5 && !this.state.showAllBrands ? <div style={{cursor: 'pointer', color: 'blue'}} onClick={() => this.setState({showAllBrands: true})}><Icon name='caret down'/>Show More</div> : null }
+          { option.options.length > 5 && this.state.showAllBrands ? <div style={{cursor: 'pointer', color: 'blue'}} onClick={() => this.setState({showAllBrands: false})}><Icon name='caret up'/>Show Less</div> : null }
         </Form.Group>
+        <Divider />
+        </div>
         )
       }
     );
