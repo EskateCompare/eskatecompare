60910beec17da9cbc9f54a14f187862ebd61e26b
mwvalen <mwillvalentine@gmail.com>
2018-09-08 01:05:49 -0400

    commented express use /public

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.use(session({ secret: 'skatecompare', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
