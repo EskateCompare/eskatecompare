b9c1adec130b617f0fd802d370389f150b7ce16c
Warren Phen <warren.phen@gmail.com>
2018-10-02 18:40:06 -0400

    added about details

--- server/client/src/components/Home/About.js
+++ server/client/src/components/Home/About.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import { Container, List, Segment, Grid, Header, Icon } from 'semantic-ui-react';
+import { Container, List, Segment, Grid, Header, Icon, Item, Image, Button } from 'semantic-ui-react';
+
+const paragraph = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
 
 class About extends Component {
   render() {
     return (
       <div>
 	      <Container style={{padding: '200px 0'}} text>
+	      <Header>About</Header>
 	        <Segment>
-	        
-	        <Header as='h1' content='About'/>
-	          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac magna ac nibh bibendum ullamcorper. Fusce molestie felis nec leo finibus, sit amet convallis leo ornare. Quisque et massa molestie erat mollis consequat. Vivamus vestibulum ut leo quis varius. Ut eget diam at dolor vulputate tempor eget vel arcu. Donec commodo tellus et urna placerat ultrices. Maecenas id purus vitae leo pretium accumsan. Nam aliquet lacinia sollicitudin. Aenean eros lacus, vulputate et convallis at, fermentum in lacus. Nunc at lectus purus. Morbi erat ligula, bibendum vel nibh ac, dignissim aliquet purus. Fusce sit amet ullamcorper dui, vel lacinia enim. Proin et tellus et felis luctus pellentesque ac vel ipsum. Nam mauris mauris, lobortis in lacinia sit amet, placerat sed dui.
-
-	Sed at rutrum metus. Suspendisse ultrices convallis lorem, ac rhoncus ante. Etiam vitae tortor dui. Nunc mi erat, facilisis sed suscipit in, laoreet sed augue. In id mi lectus. Aenean vel ligula euismod nunc iaculis ultricies. Donec tincidunt, diam vitae volutpat ultrices, purus libero feugiat turpis, ac egestas dolor nibh vel tortor. Curabitur auctor nibh vulputate mauris fringilla maximus. Proin pulvinar id neque eget luctus.
-	          
+	        	<Item.Group>
+	 						<Item>
+					      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+					      <Item.Content>
+					        <Item.Header>Michael WV</Item.Header>
+					        <Item.Meta content='Software Engineer'/>
+					        <Item.Description>{paragraph}</Item.Description>
+					        <Item.Extra>
+					          <Button circular icon='linkedin' color='linkedin' />
+										<Button circular icon='github' color='github' />
+									</Item.Extra>
+      					</Item.Content>
+						 </Item>
+    				</Item.Group>
+    			</Segment>
+					<Segment>
+						<Item.Group>
+				    	<Item>
+					      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+					      <Item.Content>
+					        <Item.Header>Warren EP</Item.Header>
+					        <Item.Meta content='Software Engineer' />
+					        <Item.Description>{paragraph}</Item.Description>
+					        <Item.Extra>
+					          <Button circular icon='linkedin' color='linkedin' />
+										<Button circular icon='github' color='github' />
+									</Item.Extra>
+					      </Item.Content>
+					    </Item>
+  					</Item.Group>
 	        </Segment>
         </Container>
       </div>
