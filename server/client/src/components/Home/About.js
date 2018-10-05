830fbe157b45b37a539a6ce425bd4d6c55af92d6
Warren Phen <warren.phen@gmail.com>
2018-10-04 21:45:04 -0400

    added bio for Warren

--- server/client/src/components/Home/About.js
+++ server/client/src/components/Home/About.js
@@ -13,7 +13,7 @@ class About extends Component {
 	        <Segment>
 	        	<Item.Group>
 	 						<Item>
-					      <Item.Image size='tiny' src='https://dgalywyr863hv.cloudfront.net/pictures/athletes/34706203/10280548/1/large.jpg' />
+					      <Item.Image avatar size='tiny' src='https://dgalywyr863hv.cloudfront.net/pictures/athletes/34706203/10280548/1/large.jpg' />
 					      <Item.Content>
 					        <Item.Header>Michael WV</Item.Header>
 					        <Item.Meta content='Software Engineer'/>
@@ -39,14 +39,24 @@ class About extends Component {
 					<Segment>
 						<Item.Group>
 				    	<Item>
-					      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+					      <Item.Image avatar size='tiny' src='https://media.licdn.com/dms/image/C5603AQFYDF28XXm1RQ/profile-displayphoto-shrink_200_200/0?e=1544054400&v=beta&t=I-cH3OIaHXTtpJ1xG67zI_1okEVm3_F6vVmXHn_lFl4' />
 					      <Item.Content>
 					        <Item.Header>Warren EP</Item.Header>
 					        <Item.Meta content='Software Engineer' />
-					        <Item.Description>{paragraph}</Item.Description>
+					        <Item.Description>
+					        	Warren created all the frontend layouts and design.  Give him some feedback at the bottom of the page! 
+					        	He's super into strength training, loves to travel and is looking for his next big adventure!
+					        </Item.Description>
 					        <Item.Extra>
-					          <Button circular icon='linkedin' color='linkedin' />
-										<Button circular icon='github' color='github' />
+					          <a href='https://www.linkedin.com/in/warren-phen-87aa8a40' target='_blank'>
+					          	<Button circular icon='linkedin' to='https://www.linkedin.com/in/warren-phen-87aa8a40' color='linkedin' />
+					          </a>
+										<a href='https://github.com/warrenphen' target='_blank'>
+											<Button circular icon='github' color='github' />
+										</a>
+										<a href='https://www.instagram.com/warren.phen' target='_blank'>
+											<Button circular icon='instagram' color='instagram' />
+										</a>
 									</Item.Extra>
 					      </Item.Content>
 					    </Item>
