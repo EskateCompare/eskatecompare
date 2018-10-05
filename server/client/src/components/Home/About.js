cf346c39714a55040442fb774fd3a674fbf13b1c
mwvalen <mwillvalentine@gmail.com>
2018-10-04 20:41:42 -0400

    About profile added Michael

--- server/client/src/components/Home/About.js
+++ server/client/src/components/Home/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, List, Segment, Grid, Header, Icon, Item, Image, Button } from 'semantic-ui-react';
 
 const paragraph = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
@@ -12,14 +13,24 @@ class About extends Component {
 	        <Segment>
 	        	<Item.Group>
 	 						<Item>
-					      <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+					      <Item.Image size='tiny' src='https://dgalywyr863hv.cloudfront.net/pictures/athletes/34706203/10280548/1/large.jpg' />
 					      <Item.Content>
 					        <Item.Header>Michael WV</Item.Header>
 					        <Item.Meta content='Software Engineer'/>
-					        <Item.Description>{paragraph}</Item.Description>
+
+					        <Item.Description>Michael is the site's backend guru. If you notice any faulty data,
+                    you know who to blame. He's also into keto, running, and wishes he bought
+                    more bitcoin. </Item.Description>
 					        <Item.Extra>
-					          <Button circular icon='linkedin' color='linkedin' />
-										<Button circular icon='github' color='github' />
+                    <a href='https://www.linkedin.com/in/michael-valentine-360a1954/' target='_blank'>
+					               <Button circular icon='linkedin' color='linkedin' />
+                    </a>
+                    <a href='https://github.com/mwvalen' target='_blank'>
+                      <Button circular icon='github' color='github' />
+                    </a>
+                    <a href='https://www.strava.com/athletes/34706203' target='_blank'>
+                      <Button circular icon='strava' color='orange' />
+                    </a>
 									</Item.Extra>
       					</Item.Content>
 						 </Item>
@@ -48,5 +59,3 @@ class About extends Component {
 }
 
 export default About;
-
-
