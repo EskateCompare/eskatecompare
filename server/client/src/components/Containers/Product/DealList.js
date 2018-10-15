91e00ca257c9db9fe1da002e89d686426aa4445e
mwvalen <mwillvalentine@gmail.com>
2018-10-15 17:55:33 -0400

    Popup on deal price with currency info

--- server/client/src/components/Containers/Product/DealList.js
+++ server/client/src/components/Containers/Product/DealList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Image, Button, Label } from 'semantic-ui-react';
+import { Table, Image, Button, Label, Popup } from 'semantic-ui-react';
 
 export default class DealList extends Component {
   renderDeals() {
@@ -11,7 +11,8 @@ export default class DealList extends Component {
         <Table.Row key={index}>
           <Table.Cell verticalAlign='middle'><Image centered src={store.logo} size='tiny' /></Table.Cell>
           <Table.Cell verticalAlign='middle'>${convertedOriginalPrice.toFixed(2)}</Table.Cell>
-          <Table.Cell verticalAlign='middle'><Label color='red' tag>${convertedSalesPrice.toFixed(2)}</Label></Table.Cell>
+          <Table.Cell verticalAlign='middle'>
+            <Popup trigger={<Label color='red' tag>${convertedSalesPrice.toFixed(2)}</Label>} content='Prices in USD' /></Table.Cell>
           <Table.Cell verticalAlign='middle'><a href={url} target="_blank"><Button primary>See Deal</Button></a></Table.Cell>
         </Table.Row>
         )
